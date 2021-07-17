e0723723e2c4832fbcdc0f7efa81c5754bae345a
albert <adaissala@gmail.com>
2021-07-17 09:39:12 -0400

    add myfle.py

--- myfle.py
+++ myfle.py
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 print("hello World, My name is Junior D!")
 Print("hello world, how are you, this is a test!")
-=======
 print("hello World, My name is junior!")
->>>>>>> f2090328415fb08a9fb36ca8d3a785f4d7203d44
+
